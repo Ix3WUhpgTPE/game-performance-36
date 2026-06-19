01619d5b86fb8dd4cddecdd4851752885f7ef038
Tomáš Novák <clipethicscourse@hotmail.com>
2026-06-19 05:24:53 +0200

    feat:define game constants and optimizations

--- constants.py
+++ constants.py
@@ -1,31 +1,36 @@
-FPS_TARGET = 60
+FPS_LIMIT = 60
 
-# Constants for game settings
-VIDEOMODE_WIDTH = 1920
-VIDEOMODE_HEIGHT = 1080
-FULLSCREEN_MODE = True
-
-# Performance-related constants
-MAX_FPS = 120
-MIN_FPS = 30
-V_SYNC = True
-
-# Audio constants
-MAX_VOLUME = 1.0
-MIN_VOLUME = 0.0
+# Game settings
+DEFAULT_RESOLUTION = (1920, 1080)
+DEFAULT_FOV = 90
 DEFAULT_VOLUME = 0.5
 
-# Physics constants
-GRAVITY = 9.81
-FRICTION = 0.5
-
 # Color constants
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
 
-# Frame timing constants
-FRAME_TIME = 1 / MAX_FPS
-SLEEP_TIME = FRAME_TIME * 0.75
+# Speed and movement constants
+PLAYER_SPEED = 5.0
+ENEMY_SPEED = 3.0
+BULLET_SPEED = 12.0
+
+# Game state constants
+STATE_MENU = 'menu'
+STATE_PLAYING = 'playing'
+STATE_PAUSED = 'paused'
+STATE_GAMEOVER = 'gameover'
+
+# Performance related constants
+MAX_ACTIVE_PARTICLES = 1000
+FRAME_TIME = 1.0 / FPS_LIMIT
+
+# Resource paths
+TEXTURE_PATH = 'assets/textures/'
+SOUND_PATH = 'assets/sounds/'
+
+# Optimization constants
+USE_SPRITE_BATCHING = True
+ENABLE_OBJECT_POOLING = True
