07d063b6869c980e5ab9c80e75278ffd2838c686
Tomáš Novák <clipethicscourse@hotmail.com>
2026-06-29 05:09:37 +0200

    test:implement rotating logger setup

--- logger.py
+++ logger.py
@@ -2,18 +2,26 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=5 * 1024 * 1024, backup_count=3):
+
+def setup_logger(log_file='game.log', max_bytes=5*1024*1024, backup_count=3):
     logger = logging.getLogger('GameLogger')
     logger.setLevel(logging.DEBUG)
 
-    if not logger.hasHandlers():
+    if not logger.handlers:
+        # Create a rotating file handler
         handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)    
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+        # Add the handler to the logger
         logger.addHandler(handler)
 
     return logger
 
+
 if __name__ == '__main__':
     log = setup_logger()
-    log.info('Logger has been set up successfully.')
+    log.debug('This is a debug message')
+    log.info('This is an info message')
+    log.warning('This is a warning message')
+    log.error('This is an error message')
+    log.critical('This is a critical message')
