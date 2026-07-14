5c05c1314606816bdfe07a7455291e71988f58a9
Tomáš Novák <clipethicscourse@hotmail.com>
2026-07-14 05:09:12 +0200

    test:set up logger with rotation

--- logger.py
+++ logger.py
@@ -1,26 +1,18 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name, log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+def setup_logger(name, log_file, level=logging.INFO):
+    logger = logging.getLogger(name)
+    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=3)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(level)
+    return logger
 
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
+# Example of using the logger
+if __name__ == '__main__':
+    log = setup_logger('GameLogger', 'game.log')
+    log.info('This is an info message.')
+    log.error('This is an error message.')
