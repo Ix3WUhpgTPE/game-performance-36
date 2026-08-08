e465b1d31f8200d0828783ceae2ce0441e90720f
Tomáš Novák <clipethicscourse@hotmail.com>
2026-08-08 05:09:12 +0200

    fix:game state management functions

--- processor.py
+++ processor.py
@@ -1,26 +1,41 @@
 import time
-import numpy as np
-from functools import lru_cache
+import random
 
 class GameProcessor:
-    def __init__(self, data):
-        self.data = np.array(data)
+    def __init__(self):
+        self.state = 'initial'
+        self.score = 0
 
-    @lru_cache(maxsize=128)
-    def calculate_performance_metrics(self, player_stats):
-        # Simulating expensive computation
-        time.sleep(0.1)
-        return np.mean(player_stats), np.std(player_stats)
+    def update_state(self, new_state):
+        if new_state in ['running', 'paused', 'stopped']:
+            self.state = new_state
+        else:
+            raise ValueError('Invalid state: ' + new_state)
 
-    def process_data(self):
-        metrics = []
-        for stats in self.data:
-            mean, std = self.calculate_performance_metrics(tuple(stats))
-            metrics.append((mean, std))
-        return metrics
+    def add_score(self, points):
+        if points < 0:
+            raise ValueError('Points cannot be negative')
+        self.score += points
 
-if __name__ == '__main__':
-    sample_data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    processor = GameProcessor(sample_data)
-    results = processor.process_data()
-    print(results)
+    def reset(self):
+        self.state = 'initial'
+        self.score = 0
+
+    def random_event(self):
+        events = ['bonus', 'minus', 'none']
+        event = random.choice(events)
+        if event == 'bonus':
+            self.add_score(10)
+            return 'You gained a bonus!'
+        elif event == 'minus':
+            self.add_score(-5)
+            return 'You lost some points!'
+        return 'No event happened.'
+
+    def simulate_game(self, duration):
+        self.update_state('running')
+        end_time = time.time() + duration
+        while time.time() < end_time:
+            print(self.random_event())
+            time.sleep(1)
+        self.update_state('stopped')
